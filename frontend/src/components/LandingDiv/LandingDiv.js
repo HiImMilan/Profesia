df066b94141bfa51e15fbbef6f7f5a772683efb0
HiImMilan <milanbabuljak@outlook.sk>
2022-02-26 23:04:13 +0100

    navbar + landing element

---
+++ frontend/src/components/LandingDiv/LandingDiv.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+let LandingDiv = () => {
+    return (
+        <div class="bg-white dark:bg-gray-800 ">
+            <div class="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-40 lg:px-8 z-20">
+                <h2 class="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
+                    <span class="block">
+                    Ara Ara, finding a job, aren&#x27;t we?
+                    </span>
+                    <span class="block text-indigo-500">
+                    Worka will help you!
+                    </span>
+                </h2>
+            </div>
+        </div>
+    )
+}
+
+export default LandingDiv;
