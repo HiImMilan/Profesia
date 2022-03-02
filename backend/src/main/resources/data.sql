551d7b285b64e84af2f5bd01ed1992fc14331fde
Milos <61538921+MilosM33@users.noreply.github.com>
2022-03-02 17:43:45 +0100

    bug fix

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,24 @@
-INSERT INTO `company`( `email`, `location`, `name`, `password`) VALUES ('@gmail.com','sadasda','sadsadas','dasdas');
-
-INSERT INTO `jobs`(`category`, `created_at`, `description`, `end_date`, `title`, `updated_at`, `company_id`) VALUES ('[value-2]','[value-3]','[value-4]','[value-5]','title','[value-7]',0);
+INSERT INTO `company`(`email`, `location`, `name`, `password`)
+VALUES ('@gmail.com', 'sadasda', 'sadsadas', 'dasdas');
+INSERT INTO `jobs`(
+        `category`,
+        `created_at`,
+        `description`,
+        `end_date`,
+        `title`,
+        `updated_at`,
+        `company_id`
+    )
+VALUES (
+        '[value-2]',
+        '2022-03-09',
+        '[value-4]',
+        '2022-03-09',
+        'title',
+        '2022-03-09',
+        0
+    );
+INSERT INTO 'categories' (`name`)
+VALUES ('IT'),
+('HR'),
+('Sales');
