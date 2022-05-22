17f006e232bdb82cb84761d9834f623b588d4fbd
Milos <61538921+MilosM33@users.noreply.github.com>
2022-05-22 20:45:57 +0200

    Toast fixes

--- frontend/src/components/DashboardView.js
+++ frontend/src/components/DashboardView.js
@@ -37,8 +37,6 @@ export default function DashboardView(props) {
           </section>
         </main>
       </section>
-
-      <Toast></Toast>
     </div>
   );
 }
--- frontend/src/components/toast/ToastPlaceholder.js
+++ frontend/src/components/toast/ToastPlaceholder.js
@@ -1,18 +1,23 @@
 import { useEffect, useState } from "react";
+import Toasty from "./Toasty";
 import Toast from "./Toast";
-export default function ToastPlaceholder(props, onRemove) {
+export default function ToastPlaceholder() {
   return (
     <div className="fixed top-0 left-0 flex flex-col">
-      {props.toasts.map((toast, index) => {
-        return (
-          <Toast
-            key={toast.id}
-            title={toast.title}
-            paragraph={toast.paragraph}
-            onChange={() => onRemove(toast.id)}
-          ></Toast>
-        );
-      })}
+      {Toasty.getInstance()
+        .Get()
+        .map((toast, index) => {
+          return (
+            <Toast
+              key={toast.id}
+              title={toast.title}
+              paragraph={toast.paragraph}
+              onChange={() => {
+                Toasty.getInstance().Remove(toast);
+              }}
+            ></Toast>
+          );
+        })}
     </div>
   );
 }
