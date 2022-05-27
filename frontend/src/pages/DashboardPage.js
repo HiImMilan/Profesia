c6f71aef3aa925da711dd24cc14d3f13597eae93
Milos <61538921+MilosM33@users.noreply.github.com>
2022-05-27 20:13:24 +0200

    ui fixes

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -5,40 +5,9 @@ import TableComponent from "../components/table/TableComponent";
 import Toast from "../components/toast/Toast";
 import Graph from "../components/graphs/Graph";
 import DashBoardCard from "../components/cards/DasboardCard";
-import { DataGrid } from "@mui/x-data-grid";
+import ApplicationsTable from "../components/table/ApplicationTable";
 
 function AdminPage() {
-  const columns = [
-    { field: "id", headerName: "ID", width: 70 },
-    { field: "firstName", headerName: "First name", width: 130 },
-    { field: "lastName", headerName: "Last name", width: 130 },
-    {
-      field: "position",
-      headerName: "Position",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
-      width: 160,
-    },
-    {
-      field: "status",
-      headerName: "Status",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
-      width: 160,
-    },
-  ];
-
-  const rows = [
-    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  ];
   return (
     <div className="App">
       <h3 className="text-2xl  text-white">Dashboard</h3>
@@ -55,23 +24,7 @@ function AdminPage() {
       </section>
 
       <section className="my-16 md:my-32">
-        <div class="h-96 w-full text-white lg:w-1/2">
-          <DataGrid
-            rows={rows}
-            columns={columns}
-            pageSize={5}
-            rowsPerPageOptions={[5]}
-            checkboxSelection
-            sx={{
-              boxShadow: 2,
-              border: 2,
-              borderColor: "primary.light",
-              "& .MuiDataGrid-cell:hover": {
-                color: "primary.main",
-              },
-            }}
-          />
-        </div>
+        <ApplicationsTable></ApplicationsTable>
       </section>
     </div>
   );
